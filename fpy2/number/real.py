2e53ee5e8eb0eb9f2598ffb241e38b4515363199
Brett Saiki <bksaiki@gmail.com>
2025-02-23 10:42:47 -0800

    implement rounding (wip)

--- fpy2/number/real.py
+++ fpy2/number/real.py
@@ -2,16 +2,16 @@
 This module defines the basic floating-point type.
 """
 
-from typing import Self
+from typing import Optional, Self
 
-from ..utils import bitmask
+from .round import RoundingMode, RoundingDirection
+from ..utils import bitmask, Ordering
 
 class Real:
     """
     The basic floating-point number.
 
     This type encodes a base-2 number in unnormalized scientific notation:
-
     `(-1)^s * 2^exp * c` where:
      - `s` is the sign;
      - `exp` is the absolute position of the least-significant bit (LSB),
@@ -22,40 +22,112 @@ class Real:
     Unlike IEEE 754, this number cannot encode infinity or NaN.
     """
 
-    s: bool
+    s: bool = False
     """is the sign negative?"""
-    exp: int
+    exp: int = 0
     """absolute position of the LSB"""
-    c: int
+    c: int = 0
     """integer significand"""
 
-    def __init__(self, s: bool, exp: int, c: int):
-        if not isinstance(s, bool):
-            raise TypeError(f"expected bool, got {type(s)} for `s`")
-        if not isinstance(exp, int):
-            raise TypeError(f"expected int, got {type(exp)} for `exp`")
-        if not isinstance(c, int):
-            raise TypeError(f"expected int, got {type(c)} for `c`")
+    def __init__(
+        self,
+        s: Optional[bool] = None,
+        exp: Optional[int] = None,
+        c: Optional[int] = None,
+        x: Optional[Self] = None,
+        e: Optional[int] = None,
+        m: Optional[int] = None,
+    ):
+        """
+        Creates a new `Real` value.
 
-        self.s = s
-        self.exp = exp
-        self.c = c
+        The sign may be optionally specified with `s`.
+        The exponent may be specified with `exp` or `e`.
+        The significand may be specified with `c` or `m` (unless `x` is given).
+        If `x` is given, any field not specified is copied from `x`.
+        """
+        # c and negative
+        if c is not None:
+            if m is not None:
+                raise ValueError(f'cannot specify both c={c} and m={m}')
+            self.c = c
+            if s is not None:
+                self.s = s
+            elif x is not None:
+                self.s = x.s
+            else:
+                self.s = type(self).s
+        elif m is not None:
+            if s is not None:
+                raise ValueError(f'cannot specify both m={m} and s={s}')
+            self.c = abs(m)
+            self.s = m < 0
+        elif x is not None:
+            self.c = x.c
+            if s is not None:
+                self.s = s
+            else:
+                self.s = x.s
+        else:
+            self.c = type(self).c
+            if s is not None:
+                self.s = s
+            else:
+                self.s = type(self).s
+
+        # exp
+        if exp is not None:
+            if e is not None:
+                raise ValueError(f'cannot specify both exp={exp} and e={e}')
+            self.exp = exp
+        elif e is not None:
+            self.exp = e - self.c.bit_length() + 1
+        elif x is not None:
+            self.exp = x.exp
+        else:
+            self.exp = type(self).exp
+
 
     def __repr__(self):
         return 'Real(s=' + repr(self.s) + ', exp=' + repr(self.exp) + ', c=' + repr(self.c) + ')'
 
+    def __hash__(self):
+        if self.c == 0:
+            return hash(self.s)
+        else:
+            return hash((self.s, self.exp, self.c))
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Real) and self.compare(other) == Ordering.EQUAL
+
+    def __lt__(self, other: Self) -> bool:
+        if not isinstance(other, Real):
+            return TypeError(f'\'<\' not supported between instances \'Real\' and \'{type(other)}\'')
+        return self.compare(other) == Ordering.LESS
+
+    def __le__(self, other: Self) -> bool:
+        if not isinstance(other, Real):
+            return TypeError(f'\'<=\' not supported between instances \'Real\' and \'{type(other)}\'')
+        return self.compare(other) != Ordering.GREATER
+
+    def __gt__(self, other: Self) -> bool:
+        if not isinstance(other, Real):
+            return TypeError(f'\'>\' not supported between instances \'Real\' and \'{type(other)}\'')
+        return self.compare(other) == Ordering.GREATER
+
+    def __ge__(self, other: Self) -> bool:
+        if not isinstance(other, Real):
+            return TypeError(f'\'>=\' not supported between instances \'Real\' and \'{type(other)}\'')
+        return self.compare(other) != Ordering.LESS
+
     @property
     def base(self):
-        """
-        Integer base of this number. Always 2.
-        """
+        """Integer base of this number. Always 2."""
         return 2
 
     @property
     def p(self):
-        """
-        Minimum number of binary digits required to represent this number.
-        """
+        """Minimum number of binary digits required to represent this number."""
         return self.c.bit_length()
 
     @property
@@ -88,15 +160,11 @@ class Real:
         return -self.c if self.s else self.c
 
     def is_zero(self) -> bool:
-        """
-        Returns whether this value represents zero.
-        """
+        """Returns whether this value represents zero."""
         return self.c == 0
 
     def is_integer(self) -> bool:
-        """
-        Returns whether this value is an integer.
-        """
+        """Returns whether this value is an integer."""
         if self.is_zero():
             return True
 
@@ -113,9 +181,7 @@ class Real:
         return fbits == 0
 
     def bit(self, n: int) -> bool:
-        """
-        Returns the value of the digit at the `n`th position as a boolean.
-        """
+        """Returns the value of the digit at the `n`th position as a boolean."""
         if not isinstance(n, int):
             raise ValueError('expected an integer', n)
 
@@ -171,8 +237,6 @@ class Real:
             lo = Real(self.s, n, 0)
             return (hi, lo)
 
-        assert self.e is not None
-
         # check if all digits are in the lower part
         if n >= self.e:
             hi = Real(self.s, n + 1, 0)
@@ -198,3 +262,187 @@ class Real:
         hi = Real(self.s, exp_hi, c_hi)
         lo = Real(self.s, exp_lo, c_lo)
         return (hi, lo)
+
+    def compare(self, other: Self) -> Ordering:
+        """Compare two `Real` values returning an `Ordering`."""
+        if not isinstance(other, Real):
+            raise TypeError(f"expected Real, got {type(other)}")
+
+        if self.c == 0:
+            if other.c == 0:
+                return Ordering.EQUAL
+            elif other.s:
+                return Ordering.GREATER
+            else:
+                return Ordering.LESS
+        elif other.c == 0:
+            if self.s:
+                return Ordering.LESS
+            else:
+                return Ordering.GREATER
+        elif self.s != other.s:
+            # non-zero signs are different
+            if self.s:
+                return Ordering.LESS
+            else:
+                return Ordering.GREATER
+        else:
+            # non-zero, signs are same
+            match Ordering.from_compare(self.e, other.e):
+                case Ordering.GREATER:
+                    # larger magnitude based on MSB
+                    cmp = Ordering.GREATER
+                case Ordering.LESS:
+                    # smaller magnitude based on MSB
+                    cmp = Ordering.LESS
+                case Ordering.EQUAL:
+                    # need to actual compare the significands
+                    n = min(self.n, other.n)
+                    c1 = self.c << (self.n - n)
+                    c2 = other.c << (other.n - n)
+                    cmp = Ordering.from_compare(c1, c2)
+
+            # adjust for the sign
+            if self.s:
+                return cmp.reverse()
+            else:
+                return cmp
+
+    def is_identical_to(self, other: Self) -> bool:
+        """Is the value encoded identically to another `Real` value?"""
+        if not isinstance(other, Real):
+            return TypeError(f'expected Real, got {type(other)}')
+        return self.s == other.s and self.exp == other.exp and self.c == other.c
+
+    def _round_params(self, max_p: Optional[int] = None, min_n: Optional[int] = None):
+        """
+        Computes rounding parameters `p` and `n`.
+
+        Given `max_p` and `min_n`, computes the actual allowable precision `p`
+        and the position of the first unrepresentable digit `n`.
+        """
+        if max_p is None:
+            p = None
+            if min_n is None:
+                raise ValueError(f'must specify {max_p} or {min_n}')
+            else:
+                # fixed-point rounding => limited by n
+                n = min_n
+        else:
+            p = max_p
+            if min_n is None:
+                # floating-point rounding => limited by fixed precision
+                n = self.e - max_p
+            else:
+                # IEEE 754 floating-point rounding
+                n = max(min_n, self.e - max_p)
+
+        return p, n
+
+    def _round_at(self, n: int):
+        """
+        Splits `self` at absolute digit position `n`.
+
+        Computes the digits of `self.c` above digit `n` and the digit
+        at position `n` as the "half" bit and a boolean to indicate
+        if any digits below position n are 1.
+        """
+
+        kept, lost = self.split(n)
+        if lost.is_zero():
+            # no bits are remaining at or below n
+            half_bit = False
+            lower_bits = False
+        elif lost.e == n:
+            # the MSB of lo is at position n
+            half_bit = (lost.c & bitmask(lost.p - 1)) != 0
+            lower_bits = (lost.c & bitmask(lost.p - 1)) != 0
+        else:
+            # the MSB of lo is below position n
+            half_bit = False
+            lower_bits = True
+
+        return kept, half_bit, lower_bits
+
+    def _round_requires_increment(
+        self,
+        kept: Self,
+        half_bit: bool,
+        lower_bits: bool,
+        nearest: bool,
+        direction: RoundingDirection,
+    ):
+        """
+        Does the rounding operation require incrementing truncated digits?
+        """
+
+
+
+    def _round_finalize(
+        self,
+        kept: Self,
+        half_bit: bool,
+        lower_bits: bool,
+        p: Optional[int],
+        rm: RoundingMode
+    ):
+        """
+        Completes the rounding operation using truncated digits
+        and rounding information.
+        """
+
+        # convert rounding mode
+        nearest, direction = rm.to_direction(kept.s)
+
+        # check if we need to increment to round correctly
+        requires_increment = self._round_requires_increment(kept, half_bit, lower_bits, nearest, direction)
+        if requires_increment:
+            # increment the significand
+            kept.c += 1
+            rounded = True
+            if p is not None and kept.c.bit_length() > p:
+                kept.c >>= 1
+                kept.exp += 1
+        else:
+            rounded = False
+
+        raise NotImplementedError
+
+
+    def round(
+        self,
+        max_p: Optional[int] = None,
+        min_n: Optional[int] = None,
+        rm: RoundingMode = RoundingMode.RNE,
+    ):
+        """
+        Rounds `self` to another value with at most `max_p` digits of precision
+        or a least absolute digit position `min_n` whichever bound is encountered
+        first, using the rounding mode specified by `rm`.
+
+        At least one of `max_p` or `min_n` must be specified:
+        `max_p >= 0` while `min_n` may be any integer.
+
+        If only `min_n` is given, rounding is performed like fixed-point
+        rounding and the resulting significand may have more than `max_p` bits
+        (any values can be clamped after this operation).
+        If only `min_p` is given, rounding is performed liked floating-point
+        without an exponent bound; the integer significand has at most
+        `max_p` digits.
+        If both are specified, rounding is performed like IEEE 754 floating-point
+        arithmetic; `min_n` takes precedence,so the value may have less than
+        `max_p` precision.
+        """
+
+        if max_p is None and min_n is None:
+            raise ValueError(f'must specify {max_p} or {min_n}')
+
+        # compute rounding parameters
+        p, n = self._round_params(max_p, min_n)
+
+        # split the significand and keep rounding bits
+        kept, half_bit, lower_bits = self._round_at(n)
+        assert p is None or kept.c.bit_length() <= p
+
+        # finalize rounding
+        return self._round_finalize(kept, half_bit, lower_bits, p, rm)
--- fpy2/utils/ordering.py
+++ fpy2/utils/ordering.py
@@ -11,3 +11,29 @@ class Ordering(IntEnum):
     LESS = -1
     EQUAL = 0
     GREATER = 1
+
+
+    @staticmethod
+    def from_compare(x, y):
+        """
+        Convert the result of a comparison to an Ordering.
+        """
+        if x < y:
+            return Ordering.LESS
+        elif x > y:
+            return Ordering.GREATER
+        elif x == y:
+            return Ordering.EQUAL
+        else:
+            raise ValueError(f"cannot compare {x} and {y}")
+
+    def reverse(self):
+        """
+        Reverse the ordering.
+        """
+        if self == Ordering.LESS:
+            return Ordering.GREATER
+        elif self == Ordering.GREATER:
+            return Ordering.LESS
+        else:
+            return Ordering.EQUAL
