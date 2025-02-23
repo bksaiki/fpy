04401ebda95d2dfbf685aac2248e356259c9e491
Brett Saiki <bksaiki@gmail.com>
2025-02-22 16:44:27 -0800

    add profiler skeletons

---
+++ fpy2/runtime/real/expr_profiler.py
@@ -0,0 +1,34 @@
+"""
+Profiler for numerical accuracy.
+"""
+
+from typing import Any, Optional
+
+from ..function import Function, Interpreter, get_default_interpreter
+from .interpreter import RealInterpreter
+
+class FunctionProfiler:
+    """
+    Per-expression profiler
+
+    Profiles each expression in a function for its numerical accuracy
+    on a set of inputs.
+    """
+
+    def profile(
+        self,
+        func: Function,
+        inputs: list[Any],
+        interpreter: Optional[Interpreter] = None,
+    ):
+        """
+        Profile the function.
+
+        If no interpreter is provided, the default interpreter is used.
+        """
+        if interpreter is None:
+            interpreter = get_default_interpreter()
+        ref_interpreter = RealInterpreter()
+
+        # TODO: howww?
+        raise NotImplementedError
