9f79c2a13bbca7dc86ecd18ca3f5afdd3a1b31a7
Wei Jun Tan <wj428@uw.edu>
2025-02-23 18:41:17 -0800

    Add rival to interpreter

--- fpy2/runtime/real/interpreter.py
+++ fpy2/runtime/real/interpreter.py
@@ -16,6 +16,10 @@ from .interval import BoolInterval, RealInterval
 from ..function import Interpreter, Function
 from ...ir import *
 
+from titanfp.titanic.gmpmath import mpfr_to_digital, mpfr
+from fpy2.runtime.real.interval import RealInterval
+from .rival_manager import RivalManager
+
 
 ScalarVal: TypeAlias = BoolInterval | RealInterval
 """Type of scalar values in FPy programs."""
@@ -31,6 +35,36 @@ TensorArg: TypeAlias = NDArray | tuple | list
 class _Interpreter(ReduceVisitor):
     """Single-use real number interpreter"""
     env: dict[NamedId, BoolInterval | RealInterval | NDArray]
+    
+    def __init__(self, p = 4):
+        self.rival = RivalManager()
+        self.p = p
+        self.rival.set_precision(p)
+
+    def _digital_to_ival(self, x: Digital):
+        """
+        Converts `x` into an interval that represents the rounding envelope of `x`,
+        i.e., the tightest set of values that would round to `x` at the
+        current precision of `x`.
+        """
+        y = x.round_new(max_p=x.p + 1)  # increase precision by 1
+        prev = y.prev_float()        # next floating-point number (toward zero)
+        next = y.next_float()        # previous floating-point number (away zero)
+        if x.negative:
+            return RealInterval(next, prev)
+        else:
+            return RealInterval(prev, next)
+
+    def _real_to_digital(self, x: str | int | float, prec: int):
+        """
+        Converts `x` into a `Digital` type without loss of accuracy.
+        Raises an exception if `x` cannot be represented exactly at the given precision.
+        """
+        rto_round = mpfr(x, prec=prec)
+        rounded = mpfr_to_digital(rto_round).round_new(max_p=prec)
+        if rounded.inexact:
+            raise ValueError(f"cannot represent {x} exactly at precision {prec}")
+        return rounded
 
     # TODO: what are the semantics of arguments
     def _arg_to_mpmf(self, arg: Any, ctx: EvalCtx):
@@ -89,46 +123,88 @@ class _Interpreter(ReduceVisitor):
         return self._lookup(e.name)
 
     def _visit_bool(self, e: Bool, ctx: Any):
-        raise NotImplementedError
+        return e.val
 
     def _visit_decnum(self, e: Decnum, ctx: EvalCtx):
-        raise NotImplementedError
+        return self.rival.eval_expr(e.val)
 
     def _visit_hexnum(self, e: Hexnum, ctx: EvalCtx):
-        raise NotImplementedError
+        return self.rival.eval_expr(e.val)
 
     def _visit_integer(self, e: Integer, ctx: EvalCtx):
-        return RealInterval.from_val(e)
+        return self.rival.eval_expr(e.val)
 
     def _visit_rational(self, e: Rational, ctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_constant(self, e: Constant, ctx: EvalCtx):
-        raise NotImplementedError
+        return self.rival.eval_expr(e.val)
 
     def _visit_digits(self, e: Digits, ctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_unknown(self, e: UnknownCall, ctx: EvalCtx):
-        raise NotImplementedError
+        self.rival.define_function(f'(f m e b) (* m (pow b e))')
+        return self.rival.eval_expr(f"f {e.m} {e.e} {e.b}")
 
     def _visit_nary_expr(self, e: NaryExpr, ctx: EvalCtx):
-        raise NotImplementedError
+        if isinstance(e, Cast):
+            x = self._visit_expr(e.children[0], ctx)
+            return x
+        elif isinstance(e, Not):
+            return self._apply_not(e, ctx)
+        elif isinstance(e, And):
+            return self._apply_and(e, ctx)
+        elif isinstance(e, Or):
+            return self._apply_or(e, ctx)
+        else:
+            print(e)
+            # TODO: try to evaluate in Rival
+            raise NotImplementedError('unknown n-ary expression', e)
+        
+    def _apply_not(self, e: Not, ctx: EvalCtx):
+        arg = self._visit_expr(e.children[0], ctx)
+        if not isinstance(arg, bool):
+            raise TypeError(f'expected a boolean argument, got {arg}')
+        return not arg
+
+    def _apply_and(self, e: And, ctx: EvalCtx):
+        args: list[bool] = []
+        for arg in e.children:
+            val = self._visit_expr(arg, ctx)
+            if not isinstance(val, bool):
+                raise TypeError(f'expected a boolean argument, got {val}')
+            args.append(val)
+        return all(args)
+
+    def _apply_or(self, e: Or, ctx: EvalCtx):
+        args: list[bool] = []
+        for arg in e.children:
+            val = self._visit_expr(arg, ctx)
+            if not isinstance(val, bool):
+                raise TypeError(f'expected a boolean argument, got {val}')
+            args.append(val)
+        return any(args)
 
     def _visit_compare(self, e: Compare, ctx: EvalCtx):
-        raise NotImplementedError
+        lhs = self._visit_expr(e.children[0], ctx)
+        for op, arg in zip(e.ops, e.children[1:]):
+            rhs = self._visit_expr(arg, ctx)
+            if not self._apply_cmp2(op, lhs, rhs):
+                return False
+            lhs = rhs
+        return True
+    
+    def _apply_cmp2(self, op: CompareOp, lhs, rhs):
+        op_map = {
+            CompareOp.EQ: "==",
+            CompareOp.NE: "!=",
+            CompareOp.LT: "<",
+            CompareOp.LE: "<=",
+            CompareOp.GT: ">",
+            CompareOp.GE: ">="
+        }
+        
+        if op not in op_map:
+            raise NotImplementedError("unknown comparison operator", op)
 
-    def _visit_tuple_expr(self, e: TupleExpr, ctx: EvalCtx):
-        raise NotImplementedError
+        self.rival.define_function(f"(f x y) ({op_map[op]} x y)")
+        return self.rival.eval_expr(f"f {lhs} {rhs}")
 
-    def _visit_tuple_ref(self, e: TupleRef, ctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_tuple_set(self, e: TupleSet, ctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_comp_expr(self, e: CompExpr, ctx: EvalCtx):
-        raise NotImplementedError
 
     def _visit_if_expr(self, e: IfExpr, ctx: EvalCtx):
         raise NotImplementedError
@@ -143,22 +219,10 @@ class _Interpreter(ReduceVisitor):
             case _:
                 raise NotImplementedError('unknown variable', stmt.var)
 
-    def _visit_tuple_assign(self, stmt: TupleAssign, ctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_ref_assign(self, stmt: RefAssign, ctx: EvalCtx):
-        raise NotImplementedError
-
     def _visit_if1_stmt(self, stmt: If1Stmt, ctx: EvalCtx):
         raise NotImplementedError
 
     def _visit_if_stmt(self, stmt: IfStmt, ctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_while_stmt(self, stmt: WhileStmt, ctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_for_stmt(self, stmt: ForStmt, ctx: EvalCtx):
         raise NotImplementedError
 
     def _visit_context(self, stmt: ContextStmt, ctx: EvalCtx):
@@ -175,12 +239,6 @@ class _Interpreter(ReduceVisitor):
     def _visit_return(self, stmt: Return, ctx: EvalCtx):
         return self._visit_expr(stmt.expr, ctx)
 
-    def _visit_phis(self, phis: list[PhiNode], lctx: EvalCtx, rctx: EvalCtx):
-        raise NotImplementedError
-
-    def _visit_loop_phis(self, phis: list[PhiNode], lctx: EvalCtx, rctx: EvalCtx):
-        raise NotImplementedError
-
     def _visit_block(self, block: Block, ctx: EvalCtx):
         for stmt in block.stmts:
             if isinstance(stmt, Return):
@@ -189,6 +247,44 @@ class _Interpreter(ReduceVisitor):
                 ctx = self._visit_statement(stmt, ctx)
 
         return None
+
+    
+    # Currently have no plan to implement functionalities below
+    def _visit_comp_expr(self, e: CompExpr, ctx: EvalCtx):
+        raise NotImplementedError
+    
+    def _visit_unknown(self, e: UnknownCall, ctx: EvalCtx):
+        raise NotImplementedError
+    
+    def _visit_constant(self, e: Constant, ctx: EvalCtx):
+        raise NotImplementedError
+    
+    def _visit_tuple_expr(self, e: TupleExpr, ctx: EvalCtx):
+        raise NotImplementedError
+
+    def _visit_tuple_ref(self, e: TupleRef, ctx: EvalCtx):
+        raise NotImplementedError
+
+    def _visit_tuple_set(self, e: TupleSet, ctx: EvalCtx):
+        raise NotImplementedError
+
+    def _visit_tuple_assign(self, stmt: TupleAssign, ctx: EvalCtx):
+        raise NotImplementedError
+
+    def _visit_ref_assign(self, stmt: RefAssign, ctx: EvalCtx):
+        raise NotImplementedError
+    
+    def _visit_while_stmt(self, stmt: WhileStmt, ctx: EvalCtx):
+        raise NotImplementedError
+
+    def _visit_for_stmt(self, stmt: ForStmt, ctx: EvalCtx):
+        raise NotImplementedError
+    
+    def _visit_phis(self, phis: list[PhiNode], lctx: EvalCtx, rctx: EvalCtx):
+        raise NotImplementedError
+
+    def _visit_loop_phis(self, phis: list[PhiNode], lctx: EvalCtx, rctx: EvalCtx):
+        raise NotImplementedError
 
     def _visit_function(self, func: FunctionDef, ctx: EvalCtx):
         raise NotImplementedError
