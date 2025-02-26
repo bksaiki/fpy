cd01ca92e84d10ca9195e828d4ca1926e37695c3
Brett Saiki <bksaiki@gmail.com>
2025-02-26 15:39:52 -0800

    fix exprs

--- fpy2/runtime/real/interpreter.py
+++ fpy2/runtime/real/interpreter.py
@@ -3,12 +3,15 @@ This module defines an FPy interpreter that uses the Rival interval library
 to compute the true real number result.
 """
 
+from fractions import Fraction
+
 from typing import Any, Optional, Sequence, TypeAlias
 
 from titanfp.arithmetic.evalctx import EvalCtx, determine_ctx
-from titanfp.arithmetic.ieee754 import ieee_ctx
+from titanfp.arithmetic.ieee754 import Float, ieee_ctx
 from titanfp.titanic.ndarray import NDArray
 from titanfp.titanic.digital import Digital
+from titanfp.titanic.ops import RM
 
 from .interval import BoolInterval, RealInterval
 from .rival_manager import RivalManager, InsufficientPrecisionError
@@ -84,11 +87,11 @@ _method_table: dict[str, str] = {
 
 class _Interpreter(ReduceVisitor):
     """Single-use real number interpreter"""
-    env: dict[NamedId, ScalarVal]
+    env: dict[NamedId, str | RealInterval | bool]
     
-    def __init__(self):
+    def __init__(self, logging: bool = False):
         self.env = {}
-        self.rival = RivalManager()
+        self.rival = RivalManager(logging)
         self.rival.set_print_ival(True)
 
     def _arg_to_mpmf(self, arg: Any, ctx: EvalCtx):
@@ -149,7 +152,7 @@ class _Interpreter(ReduceVisitor):
         return name.base # We return the name rather than the value in expression
 
     def _visit_var(self, e: Var, ctx: EvalCtx):
-        return self._lookup(e.name)
+        return e.name
 
     def _visit_bool(self, e: Bool, ctx: Any):
         return e.val
@@ -164,10 +167,11 @@ class _Interpreter(ReduceVisitor):
         return str(e.val)
 
     def _visit_rational(self, e: Rational, ctx: EvalCtx):
-        return f'(/ {e.p} {e.q})'
+        return f'{e.p}/{e.q}'
 
     def _visit_digits(self, e: Digits, ctx: EvalCtx):
-        return f'(* {e.m} (pow {e.b} {e.e}))'
+        x = Fraction(e.b) ** e.e
+        return str(e.m * x)
 
     def _visit_nary_expr(self, e: NaryExpr, ctx: EvalCtx):
         if e.name in _method_table:
@@ -182,75 +186,64 @@ class _Interpreter(ReduceVisitor):
             return self._apply_or(e, ctx)
         else:
             raise NotImplementedError('unknown n-ary expression', e)
-    
+
     def _apply_method(self, e: NaryExpr, ctx: EvalCtx):
         fn = _method_table[e.name]
-        args: list[str] = []
-        for arg in e.children:
-            val = self._visit_expr(arg, ctx)
-            if not isinstance(val, str):
-                raise TypeError(f'expected a string argument for {e.name}, got {val} with type {type(val)}.')
-            args.append(val)
-
+        args = [self._visit_expr(arg, ctx) for arg in e.children]
         arg_values_str = " ".join(map(str, args))
         return f"({fn} {arg_values_str})"
 
-        
     def _apply_not(self, e: Not, ctx: EvalCtx):
         arg = self._visit_expr(e.children[0], ctx)
-        if not isinstance(arg, bool):
-            raise TypeError(f'expected a boolean argument, got {arg}')
-        return not arg
+        return f'(not {arg})'
+
+    def _nary_to_2ary(self, op: str, args: list) -> str:
+        if len(args) == 1:
+            return args[0]
+        elif len(args) == 2:
+            return f'({op} {" ".join(args)})'
+        else:
+            return f'({op} {self._nary_to_2ary(op, args[:-1])} {args[-1]})'
 
     def _apply_and(self, e: And, ctx: EvalCtx):
-        args: list[bool] = []
-        for arg in e.children:
-            val = self._visit_expr(arg, ctx)
-            if not isinstance(val, bool):
-                raise TypeError(f'expected a boolean argument, got {val}')
-            args.append(val)
-        return all(args)
+        args = [self._visit_expr(arg, ctx) for arg in e.children]
+        return self._nary_to_2ary('and', args)
 
     def _apply_or(self, e: Or, ctx: EvalCtx):
-        args: list[bool] = []
-        for arg in e.children:
-            val = self._visit_expr(arg, ctx)
-            if not isinstance(val, bool):
-                raise TypeError(f'expected a boolean argument, got {val}')
-            args.append(val)
-        return any(args)
+        args = [self._visit_expr(arg, ctx) for arg in e.children]
+        return self._nary_to_2ary('or', args)
+
+    def _apply_cmp2(self, op: CompareOp, lhs, rhs):
+        match op:
+            case CompareOp.EQ:
+                return f'(== {lhs} {rhs})'
+            case CompareOp.NE:
+                return f'(!= {lhs} {rhs})'
+            case CompareOp.LT:
+                return f'(< {lhs} {rhs})'
+            case CompareOp.LE:
+                return f'(<= {lhs} {rhs})'
+            case CompareOp.GT:
+                return f'(> {lhs} {rhs})'
+            case CompareOp.GE:
+                return f'(>= {lhs} {rhs})'
+            case _:
+                raise NotImplementedError('unknown comparison operator', op)
 
     def _visit_compare(self, e: Compare, ctx: EvalCtx):
+        args: list[str] = []
         lhs = self._visit_expr(e.children[0], ctx)
         for op, arg in zip(e.ops, e.children[1:]):
             rhs = self._visit_expr(arg, ctx)
-            if not self._apply_cmp2(op, lhs, rhs):
-                return False
+            args.append(self._apply_cmp2(op, lhs, rhs))
             lhs = rhs
-        return True
-    
-    def _apply_cmp2(self, op: CompareOp, lhs, rhs):
-        op_map = {
-            CompareOp.EQ: "==",
-            CompareOp.NE: "!=",
-            CompareOp.LT: "<",
-            CompareOp.LE: "<=",
-            CompareOp.GT: ">",
-            CompareOp.GE: ">="
-        }
-        
-        if op not in op_map:
-            raise NotImplementedError("unknown comparison operator", op)
-        
-        self.rival.define_function(f"(f {' '.join(map(str, self.env.keys()))}) ({op_map[op]} {lhs} {rhs})")
-        return self.rival.eval_expr(f"f {' '.join(map(str, self.env.values()))}")
-
+        return self._nary_to_2ary('and', args)
 
     def _visit_if_expr(self, e: IfExpr, ctx: EvalCtx):
         cond = self._visit_expr(e.cond, ctx)
-        if not isinstance(cond, bool):
-            raise TypeError(f'expected a boolean, got {cond}')
-        return self._visit_expr(e.ift if cond else e.iff, ctx)
+        ift = self._visit_expr(e.ift, ctx)
+        iff = self._visit_expr(e.iff, ctx)
+        return f'(if {cond} {ift} {iff})'
 
     def _visit_var_assign(self, stmt: VarAssign, ctx: EvalCtx):
         match stmt.var:
@@ -298,21 +291,47 @@ class _Interpreter(ReduceVisitor):
             raise AssertionError(stmt.msg)
         return ctx
 
-    def _visit_return(self, stmt: Return, ctx: EvalCtx):
-        return self._visit_rival(stmt.expr, ctx)
+    def _interval_to_real(self, val: RealInterval) -> float:
+        # round the endpoints inwards, see if they converge
+        lo = Float(x=val.lo, ctx=ieee_ctx(11, 64, RM.RAZ))
+        hi = Float(x=val.hi, ctx=ieee_ctx(11, 64, RM.RTZ))
+        if lo != hi:
+            raise ValueError(f'interval {val} did not converge')
+        else:
+            return lo
+
+    def _visit_return(self, stmt: Return, ctx: EvalCtx) -> bool | float:
+        # since we are returning we actually want a value
+        val = self._visit_rival(stmt.expr, ctx)
+        match val:
+            case bool():
+                return val
+            case str():
+                val = self.rival.eval_expr(val)
+                if isinstance(val, RealInterval):
+                    return self._interval_to_real(val)
+                else:
+                    return val
+            case RealInterval():
+                return self._interval_to_real(val)
+            case _:
+                raise NotImplementedError('unreachable', val)
 
     def _visit_rival(self, expr: Expr, ctx: EvalCtx):
         val = self._visit_expr(expr, ctx)
-        if isinstance(val, str) and not isinstance(expr, Digits) and val.startswith('('):
-            # Expression to be evaluated by Rival
+        if isinstance(val, NamedId):
+            # variable name
+            return self.env[val]
+        elif isinstance(val, bool):
+            # boolean
+            return val
+        elif val.startswith('('):
+            # expression to be evaluated by Rival
             self.rival.define_function(f"(f {' '.join(map(str, self.env.keys()))}) {val}")
-            val = self.rival.eval_expr(f"f {' '.join(map(str, self.env.values()))}")
+            return self.rival.eval_expr(f"f {' '.join(map(str, self.env.values()))}")
         else:
-            # It might be a variable name. If so get the value from it
-            for k, v in self.env.items():
-                if str(k) == val:
-                    val = v
-        return val
+            # numerical constant
+            return val
 
     def _visit_block(self, block: Block, ctx: EvalCtx):
         for stmt in block.stmts:
@@ -342,7 +361,7 @@ class _Interpreter(ReduceVisitor):
                 raise TypeError(f'expected a boolean, got {cond}')
 
     # override for typing
-    def _visit_expr(self, e: Expr, ctx: EvalCtx) -> str:
+    def _visit_expr(self, e: Expr, ctx: EvalCtx) -> NamedId | str | bool:
         return super()._visit_expr(e, ctx)
 
     # Currently have no plan to implement functionalities below
@@ -393,6 +412,12 @@ class RealInterpreter(Interpreter):
     More information on the Rival library and the Herbie project can
     be found here: https://herbie.uwplse.org/.
     """
+    
+    logging: bool
+    """enable logging?"""
+
+    def __init__(self, logging: bool = False):
+        self.logging = logging
 
     def eval(
         self,
@@ -402,4 +427,4 @@ class RealInterpreter(Interpreter):
     ):
         if not isinstance(func, Function):
             raise TypeError(f'Expected Function, got {func}')
-        return _Interpreter().eval(func.ir, args, ctx)
+        return _Interpreter(logging=self.logging).eval(func.ir, args, ctx)
