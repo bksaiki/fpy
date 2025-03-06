8da1750a3022cff09dd59311276aab15d9288835
Brett Saiki <bksaiki@gmail.com>
2025-03-06 12:00:15 -0800

    working range table

--- fpy2/utils/__init__.py
+++ fpy2/utils/__init__.py
@@ -3,6 +3,7 @@
 from .compare import CompareOp
 from .defaults import default_repr
 from .error import FPySyntaxError, raise_type_error
+from .fraction import digits_to_fraction, hexnum_to_fraction
 from .gensym import Gensym
 from .identifier import Id, NamedId, UnderscoreId, SourceId
 from .location import Location
