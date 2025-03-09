654956d8b94ddbf3bfca9020c8419e78d0afb4af
Brett Saiki <bksaiki@gmail.com>
2025-03-09 16:19:41 -0700

    add rounding context type

---
+++ fpy2/number/context.py
@@ -0,0 +1,54 @@
+"""
+This module defines the rounding context type.
+"""
+
+from abc import ABC, abstractmethod
+
+class Context(ABC):
+    """
+    Rounding context type.
+
+    Most mathematical operators on digital numbers
+    can be decomposed into two steps:
+
+    1. a mathematically-correct operation over real numbers,
+    interpreting digital numbers as real numbers;
+    2. a rounding operation to limit the number significant digits
+    and decide how the "lost" digits will affect the final output.
+
+    Thus, rounding enforces a particular "format" for digital numbers,
+    but they should just be considered unbounded real numbers
+    when in isolation. The characteristics of the rounding operation are
+    summarized by this type.
+    """
+
+    @abstractmethod
+    def round(self, x):
+        """Rounds any digital number according to this context."""
+        raise NotImplementedError('virtual method')
+
+
+class FixedSizeContext(Context):
+    """
+    Rounding context for formats encodable in a fixed size.
+
+    Most common number formats fall under this category.
+    These formats define a way to encode a number in memory.
+    """
+
+    @abstractmethod
+    def encode(self, x) -> int:
+        """
+        Encodes a digital number constructed under this context as a bitstring.
+        This operation is context dependent.
+        """
+        raise NotImplementedError('virtual method')
+
+    @abstractmethod
+    def decode(self, x: int):
+        """
+        Decodes a bitstring as a a digital number constructed under this context.
+        This operation is context dependent.
+        """
+        raise NotImplementedError('virtual method')
+
