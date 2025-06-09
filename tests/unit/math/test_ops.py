a39f93b89d2ec05e5cd57ae755546e5447460155
Brett Saiki <bksaiki@gmail.com>
2025-06-09 16:09:36 -0700

    integer add math tests

--- tests/unit/math/test_ops.py
+++ tests/unit/math/test_ops.py
@@ -1,7 +1,7 @@
 import random
 import unittest
 
-from fpy2 import IEEEContext, MPFixedContext, RM, FP64, FP32, FP16
+from fpy2 import IEEEContext, MPFixedContext, FixedContext, RM, OF, FP64, FP32, FP16
 from fpy2.math import *
 
 _unary_ops = [
@@ -174,6 +174,74 @@ class MathIntegerNoExceptTestCase(unittest.TestCase):
                     op(x, y, ctx)
 
     def test_fuzz_ternary(self, num_inputs: int = 256):
+        for op in _ternary_ops:
+            max_value = self._max_integer.get(op, self._default_max_integer)
+            for rm in _rms:
+                ctx = MPFixedContext(-1, rm, enable_nan=True, enable_inf=True)
+                for _ in range(num_inputs):
+                    # sample point
+                    i = random.randint(0, max_value) * random.choice([-1, 1])
+                    j = random.randint(0, max_value) * random.choice([-1, 1])
+                    k = random.randint(0,max_value) * random.choice([-1, 1])
+                    x = Float.from_int(i, ctx)
+                    y = Float.from_int(j, ctx)
+                    z = Float.from_int(k, ctx)
+                    # evaluate
+                    op(x, y, z, ctx)
+
+class MathInt64NoExceptTestCase(unittest.TestCase):
+    """
+    Fuzz testing for integer operations under `fpy2.math`.
+
+    Ensures that the integer operations in `fpy2.math`
+    don't throw an exception for randomly sampled inputs.
+    """
+
+    _default_max_integer = 1 << 62
+    _max_integer = {
+        exp: 1 << 8,
+        exp2: 1 << 8,
+        exp10: 1 << 8,
+        expm1: 1 << 8,
+        pow: 1 << 4,
+        cosh: 1 << 8,
+        sinh: 1 << 8,
+        tgamma: 1 << 4,
+    }
+
+    def test_fuzz_unary(self, num_inputs: int = 256):
+        INT64 = FixedContext(True, 0, 64, RM.RTZ, OF.WRAP)
+        INT64 = FixedContext(True, 0, 64, RM.RTZ, OF.WRAP, nan_value=INT64.maxval(s=True), inf_value=INT64.maxval(s=True))
+        for op in _unary_ops:
+            max_value = self._max_integer.get(op, self._default_max_integer)
+            for rm in _rms:
+                for _ in range(num_inputs):
+                    # sample point
+                    i = random.randint(0, max_value) * random.choice([-1, 1])
+                    i *= random.choice([-1, 1])
+                    x = Float.from_int(i, INT64)
+                    # evaluate
+                    op(x, INT64)
+
+    def test_fuzz_binary(self, num_inputs: int = 256):
+        INT64 = FixedContext(True, 0, 64, RM.RTZ, OF.WRAP)
+        INT64 = FixedContext(True, 0, 64, RM.RTZ, OF.WRAP, nan_value=INT64.maxval(s=True), inf_value=INT64.maxval(s=True))
+        for op in _binary_ops:
+            max_value = self._max_integer.get(op, self._default_max_integer)
+            for rm in _rms:
+                ctx = MPFixedContext(-1, rm, enable_nan=True, enable_inf=True)
+                for _ in range(num_inputs):
+                    # sample point
+                    i = random.randint(0, max_value) * random.choice([-1, 1])
+                    j = random.randint(0, max_value) * random.choice([-1, 1])
+                    x = Float.from_int(i, ctx)
+                    y = Float.from_int(j, ctx)
+                    # evaluate
+                    op(x, y, ctx)
+
+    def test_fuzz_ternary(self, num_inputs: int = 256):
+        INT64 = FixedContext(True, 0, 64, RM.RTZ, OF.WRAP)
+        INT64 = FixedContext(True, 0, 64, RM.RTZ, OF.WRAP, nan_value=INT64.maxval(s=True), inf_value=INT64.maxval(s=True))
         for op in _ternary_ops:
             max_value = self._max_integer.get(op, self._default_max_integer)
             for rm in _rms:
