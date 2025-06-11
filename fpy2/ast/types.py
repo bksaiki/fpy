2088d40f41f4b9ea1ffd08b3902e70041d1e4efb
Brett Saiki <bksaiki@gmail.com>
2025-06-11 10:41:20 -0700

    add FPy types

---
+++ fpy2/ast/types.py
@@ -0,0 +1,93 @@
+"""
+FPy types.
+"""
+
+from typing import Sequence
+
+from ..utils import NamedId, default_repr
+from ..number import Context
+
+@default_repr
+class Type:
+    """Base class for all FPy types."""
+    pass
+
+class ScalarType(Type):
+    """Base class for scalar types."""
+    pass
+
+class TensorType(Type):
+    """Base class for tensor types."""
+    pass
+
+
+class BoolType(ScalarType):
+    """Boolean type."""
+    def __eq__(self, other):
+        return isinstance(other, BoolType)
+
+    def __hash__(self):
+        return hash(())
+
+class RealType(ScalarType):
+    """Real number type."""
+    ctx: Context | NamedId
+
+    def __init__(self, ctx: Context | NamedId):
+        self.ctx = ctx
+
+    def __eq__(self, other):
+        # TODO: how to check if two contexts are equivalent?
+        return isinstance(other, RealType) and self.ctx == other.ctx
+
+    def __hash__(self):
+        return hash((self.ctx,))
+
+class TupleType(Type):
+    """Tuple type."""
+    types: tuple[Type | NamedId, ...]
+
+    def __init__(self, *types: Type):
+        self.types = types
+
+    def __eq__(self, other):
+        return isinstance(other, TupleType) and self.types == other.types
+
+    def __hash__(self):
+        return hash(self.types)
+
+class SizedTensorType(Type):
+    dims: list[int | NamedId]
+    elt: Type
+
+    def __init__(self, dims: Sequence[int | NamedId], elt: Type):
+        self.dims = list(dims)
+        self.elt = elt
+
+    def __eq__(self, other):
+        return (isinstance(other, SizedTensorType) and
+                self.dims == other.dims and
+                self.elt == other.elt)
+
+    def __hash__(self):
+        return hash((tuple(self.dims), self.elt))
+
+class FunctionType(Type):
+    """Function type."""
+    ctx: Context | NamedId
+    args: tuple[Type | NamedId, ...]
+    ret: Type | NamedId
+
+    def __init__(self, ctx: Context | NamedId, args: Sequence[Type | NamedId], ret: Type | NamedId):
+        self.ctx = ctx
+        self.args = tuple(args)
+        self.ret = ret
+
+    def __eq__(self, other):
+        return (isinstance(other, FunctionType) and
+                self.ctx == other.ctx and
+                self.args == other.args and
+                self.ret == other.ret)
+
+    def __hash__(self):
+        return hash((self.ctx, self.args, self.ret))
