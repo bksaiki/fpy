73409cf15531b8e3195ab74aa8fc973e2c81035c
Brett Saiki <bksaiki@gmail.com>
2025-07-06 15:04:34 -0700

    add test for `round_at`

--- tests/unit/number/test_round.py
+++ tests/unit/number/test_round.py
@@ -16,7 +16,8 @@ def _all_contexts():
         fp.MPSFloatContext(24, -127, fp.RM.RNE),
         fp.MPBFloatContext(24, -127, fp.RealFloat.from_int(2 ** 100).normalize(24), fp.RM.RNE),
         fp.MPFixedContext(-8, fp.RM.RNE),
-        fp.MPBFixedContext(-8, fp.RealFloat.from_int(2 ** 30), fp.RM.RNE, fp.OF.SATURATE)
+        fp.MPBFixedContext(-8, fp.RealFloat.from_int(2 ** 30), fp.RM.RNE, fp.OF.SATURATE),
+        fp.RealContext()
     ]
 
     return common + inst
@@ -35,14 +36,43 @@ class ContextRoundTestCase(unittest.TestCase):
     @given(st.fractions())
     def test_round_fraction(self, x: Fraction):
         for ctx in _all_contexts():
-            y = ctx.round(x)
-            self.assertIsInstance(y, fp.Float)
-            self.assertIs(y.ctx, ctx)
+            if not isinstance(ctx, fp.RealContext):
+                y = ctx.round(x)
+                self.assertIsInstance(y, fp.Float)
+                self.assertIs(y.ctx, ctx)
 
     @given(st.floats(allow_nan=True, allow_infinity=True, allow_subnormal=True))
-    def test_round_float(self, x):
+    def test_round_float(self, x: float):
         for ctx in _all_contexts():
             if not isinstance(ctx, fp.MPFixedContext | fp.MPBFixedContext | fp.FixedContext):
                 y = ctx.round(x)
+                self.assertIsInstance(y, fp.Float)
+                self.assertIs(y.ctx, ctx)
+
+
+class ContextRoundAtTestCase(unittest.TestCase):
+    """Testing `Context.round_at()`"""
+
+    @given(st.integers(), st.integers())
+    def test_round_int(self, x: int, n: int):
+        for ctx in _all_contexts():
+            if not isinstance(ctx, fp.RealContext):
+                y = ctx.round_at(x, n)
+                self.assertIsInstance(y, fp.Float)
+                self.assertIs(y.ctx, ctx)
+
+    @given(st.fractions(), st.integers())
+    def test_round_fraction(self, x: Fraction,n: int):
+        for ctx in _all_contexts():
+            if not isinstance(ctx, fp.RealContext):
+                y = ctx.round_at(x, n)
+                self.assertIsInstance(y, fp.Float)
+                self.assertIs(y.ctx, ctx)
+
+    @given(st.floats(allow_nan=True, allow_infinity=True, allow_subnormal=True), st.integers())
+    def test_round_float(self, x: float, n: int):
+        for ctx in _all_contexts():
+            if not isinstance(ctx, fp.MPFixedContext | fp.MPBFixedContext | fp.FixedContext | fp.RealContext):
+                y = ctx.round_at(x, n)
                 self.assertIsInstance(y, fp.Float)
                 self.assertIs(y.ctx, ctx)
