a239395bdb721308d77762072a6d7b344a6bfa3b
Brett Saiki <bksaiki@gmail.com>
2025-09-20 18:18:22 -0700

    add definition analysis

--- fpy2/analysis/__init__.py
+++ fpy2/analysis/__init__.py
@@ -1,6 +1,7 @@
 """Program analyses for FPy programs"""
 
 from .context_infer import ContextInfer, ContextAnalysis, ContextInferError, TypeContext
+from .defs import DefinitionAnalysis
 from .define_use import (
     DefineUse, DefineUseAnalysis, Definition, DefinitionCtx,
     AssignDef, PhiDef,
