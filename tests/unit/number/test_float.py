bcf2948eb0e88c1b291caf9c98b9882bfe0b6267
Brett Saiki <bksaiki@gmail.com>
2025-10-01 16:57:14 -0700

    `RealFloat`: fix `from_rational()`; add unit tests

--- tests/unit/number/test_float.py
+++ tests/unit/number/test_float.py
@@ -18,3 +18,32 @@ class TestFloatMethods(unittest.TestCase):
         self.assertIsInstance(y, fp.Float)
         z = float(y)
         self.assertTrue(math.isnan(z) if math.isnan(x) else z == x)
+
+    @given(st.fractions(min_value=-1, max_value=1, max_denominator=1000))
+    def test_from_rational(self, x):
+        if fp.utils.is_dyadic(x):
+            y = fp.Float.from_rational(x)
+            self.assertIsInstance(y, fp.Float)
+            self.assertEqual(x, y)
+        else:
+            with self.assertRaises(ValueError):
+                fp.Float.from_rational(x)
+
+    @given(st.integers())
+    def test_as_int(self, x):
+        y = fp.Float.from_int(x)
+        self.assertEqual(x, int(y))
+
+    @given(st.floats(allow_nan=False, allow_infinity=False, allow_subnormal=True))
+    def test_as_float(self, x):
+        y = fp.Float.from_float(x)
+        self.assertEqual(x, float(y))
+
+    @given(st.fractions(min_value=-1, max_value=1, max_denominator=1000))
+    def test_as_rational(self, x):
+        if fp.utils.is_dyadic(x):
+            y = fp.Float.from_rational(x)
+            self.assertEqual(x, y.as_rational())
+        else:
+            with self.assertRaises(ValueError):
+                fp.Float.from_rational(x)
