ef1cf3a6aeaa1644ba77a79e672dac1fa4aed41e
Brett Saiki <bksaiki@gmail.com>
2025-11-07 10:29:57 -0800

    simplify doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,3 @@
-import sys
+from sphinx_pyproject import SphinxConfig
 
-from pathlib import Path
-
-top_path = Path('..', '..').resolve()
-sys.path.insert(0, str(top_path))
-
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'fpy2'
-copyright = '2025, Brett Saiki'
-author = 'Brett Saiki'
-release = '0.1.1'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+config = SphinxConfig('../../pyproject.toml', globalns=globals())
