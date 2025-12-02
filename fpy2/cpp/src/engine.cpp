fd9161dacf7416b252a5a773b05fe41463824197
Brett Saiki <bksaiki@gmail.com>
2025-12-01 22:08:42 -0800

    cpp: intrinsics to control fp

--- fpy2/cpp/src/engine.cpp
+++ fpy2/cpp/src/engine.cpp
@@ -1,38 +1,30 @@
 #include <bit>
-#include <cfenv>
 #include <cmath>
-#include <xmmintrin.h>
 
+#include "fpy/arch.hpp"
 #include "fpy/engine.hpp"
 
 namespace fpy {
 
 namespace engine {
 
-/// @brief Clears all floating-point exceptions.
-inline void clear_exceptions() {
-    // clear all exceptions
-    unsigned int mx = _mm_getcsr();
-    _mm_setcsr(mx & ~FE_ALL_EXCEPT);
+static double finalize(double result, int fexps) {
+    // check if overflow or underflow occurred
+    FPY_ASSERT(
+        !(fexps & (arch::EXCEPT_OVERFLOW | arch::EXCEPT_UNDERFLOW)),
+        "rto_add: overflow or underflow occurred"
+    );
+
+    // check inexactness
+    if (fexps & arch::EXCEPT_INEXACT) {
+        uint64_t b = std::bit_cast<uint64_t>(result);
+        b |= 1; // set LSB
+        result = std::bit_cast<double>(b);
+    }
+
+    return result;
 }
 
-/// @brief Sets the current rounding mode.
-inline void set_rm(int rm) {
-    std::fesetround(rm);
-}
-
-/// @brief Sets the rounding mode to round-to-zero (RTZ).
-/// Returns the old rounding mode.
-inline int set_rtz() {
-    const int old_rm = std::fegetround();
-    set_rm(FE_TOWARDZERO);
-    return old_rm;
-}
-
-/// @brief Loads the current floating-point exceptions.
-inline unsigned int load_exceptions() {
-    return _mm_getcsr();
-}
 
 double add(double x, double y, prec_t p) {
     // double-precision only guarantees 53 bits of precision
@@ -42,33 +34,16 @@ double add(double x, double y, prec_t p) {
     );
 
     // prepare floating-point environment
-    clear_exceptions();
-    const int rm = set_rtz();
+    const auto old_mode = arch::prepare_rto();
 
     // perform addition in RTZ mode
     double result = x + y;
 
-    // load exceptions
-    const auto fexps = load_exceptions();
+    // load exceptions and reset rounding mode
+    const auto fexps = arch::rto_status(old_mode);
 
-    // reset rounding mode
-    set_rm(rm);
-
-    // check if overflow or underflow occurred
-    FPY_ASSERT(
-        !(fexps & (FE_OVERFLOW | FE_UNDERFLOW)),
-        "rto_add: overflow or underflow occurred"
-    );
-
-    // check inexactness
-    if (fexps & FE_INEXACT) {
-        // set the LSB to emulate round-to-odd
-        uint64_t b = std::bit_cast<uint64_t>(result);
-        b |= 1; // set LSB
-        result = std::bit_cast<double>(b);
-    }
-
-    return result;
+    // finalize result
+    return finalize(result, fexps);
 }
 
 double sub(double x, double y, prec_t p) {
@@ -79,32 +54,16 @@ double sub(double x, double y, prec_t p) {
     );
 
     // prepare floating-point environment
-    clear_exceptions();
-    const int rm = set_rtz();
+    const auto old_mode = arch::prepare_rto();
 
     // perform subtraction in RTZ mode
     double result = x - y;
 
-    // load exceptions
-    const auto fexps = load_exceptions();
+    // load exceptions and reset rounding mode
+    const auto fexps = arch::rto_status(old_mode);
 
-    // reset rounding mode
-    set_rm(rm);
-
-    // check if overflow or underflow occurred
-    FPY_ASSERT(
-        !(fexps & (FE_OVERFLOW | FE_UNDERFLOW)),
-        "sub: overflow or underflow occurred"
-    );
-
-    // check inexactness
-    if (fexps & FE_INEXACT) {
-        uint64_t b = std::bit_cast<uint64_t>(result);
-        b |= 1; // set LSB
-        result = std::bit_cast<double>(b);
-    }
-
-    return result;
+    // finalize result
+    return finalize(result, fexps);
 }
 
 double mul(double x, double y, prec_t p) {
@@ -115,32 +74,16 @@ double mul(double x, double y, prec_t p) {
     );
 
     // prepare floating-point environment
-    clear_exceptions();
-    const int rm = set_rtz();
+    const auto old_mode = arch::prepare_rto();
 
     // perform multiplication in RTZ mode
     double result = x * y;
 
-    // load exceptions
-    const auto fexps = load_exceptions();
+    // load exceptions and reset rounding mode
+    const auto fexps = arch::rto_status(old_mode);
 
-    // reset rounding mode
-    set_rm(rm);
-
-    // check if overflow or underflow occurred
-    FPY_ASSERT(
-        !(fexps & (FE_OVERFLOW | FE_UNDERFLOW)),
-        "mul: overflow or underflow occurred"
-    );
-
-    // check inexactness
-    if (fexps & FE_INEXACT) {
-        uint64_t b = std::bit_cast<uint64_t>(result);
-        b |= 1; // set LSB
-        result = std::bit_cast<double>(b);
-    }
-
-    return result;
+    // finalize result
+    return finalize(result, fexps);
 }
 
 double div(double x, double y, prec_t p) {
@@ -151,32 +94,16 @@ double div(double x, double y, prec_t p) {
     );
 
     // prepare floating-point environment
-    clear_exceptions();
-    const int rm = set_rtz();
+    const auto old_mode = arch::prepare_rto();
 
     // perform division in RTZ mode
     double result = x / y;
 
-    // load exceptions
-    const auto fexps = load_exceptions();
+    // load exceptions and reset rounding mode
+    const auto fexps = arch::rto_status(old_mode);
 
-    // reset rounding mode
-    set_rm(rm);
-
-    // check if overflow or underflow occurred
-    FPY_ASSERT(
-        !(fexps & (FE_OVERFLOW | FE_UNDERFLOW)),
-        "div: overflow or underflow occurred"
-    );
-
-    // check inexactness
-    if (fexps & FE_INEXACT) {
-        uint64_t b = std::bit_cast<uint64_t>(result);
-        b |= 1; // set LSB
-        result = std::bit_cast<double>(b);
-    }
-
-    return result;
+    // finalize result
+    return finalize(result, fexps);
 }
 
 double sqrt(double x, prec_t p) {
@@ -187,32 +114,16 @@ double sqrt(double x, prec_t p) {
     );
 
     // prepare floating-point environment
-    clear_exceptions();
-    const int rm = set_rtz();
+    const auto old_mode = arch::prepare_rto();
 
     // perform square root in RTZ mode
     double result = std::sqrt(x);
 
-    // load exceptions
-    const auto fexps = load_exceptions();
+    // load exceptions and reset rounding mode
+    const auto fexps = arch::rto_status(old_mode);
 
-    // reset rounding mode
-    set_rm(rm);
-
-    // check if overflow or underflow occurred
-    FPY_ASSERT(
-        !(fexps & (FE_OVERFLOW | FE_UNDERFLOW)),
-        "sqrt: overflow or underflow occurred"
-    );
-
-    // check inexactness
-    if (fexps & FE_INEXACT) {
-        uint64_t b = std::bit_cast<uint64_t>(result);
-        b |= 1; // set LSB
-        result = std::bit_cast<double>(b);
-    }
-
-    return result;
+    // finalize result
+    return finalize(result, fexps);
 }
 
 double fma(double x, double y, double z, prec_t p) {
@@ -223,32 +134,16 @@ double fma(double x, double y, double z, prec_t p) {
     );
 
     // prepare floating-point environment
-    clear_exceptions();
-    const int rm = set_rtz();
+    const auto old_mode = arch::prepare_rto();
 
     // perform fused multiply-add in RTZ mode
     double result = std::fma(x, y, z);
 
-    // load exceptions
-    const auto fexps = load_exceptions();
+    // load exceptions and reset rounding mode
+    const auto fexps = arch::rto_status(old_mode);
 
-    // reset rounding mode
-    set_rm(rm);
-
-    // check if overflow or underflow occurred
-    FPY_ASSERT(
-        !(fexps & (FE_OVERFLOW | FE_UNDERFLOW)),
-        "fma: overflow or underflow occurred"
-    );
-
-    // check inexactness
-    if (fexps & FE_INEXACT) {
-        uint64_t b = std::bit_cast<uint64_t>(result);
-        b |= 1; // set LSB
-        result = std::bit_cast<double>(b);
-    }
-
-    return result;
+    // finalize result
+    return finalize(result, fexps);
 }
 
 } // end namespace engine
