6d5c4d427abb3bb53bddf208c87c2d46a4f723b0
Brett Saiki <bksaiki@gmail.com>
2026-01-14 10:17:46 -0800

    cpp: add fixed-point rounding

--- fpy-cpp/tests/test_round_opt.cpp
+++ fpy-cpp/tests/test_round_opt.cpp
@@ -27,6 +27,27 @@ TEST(RoundOpt, TestRoundExamples) {
     EXPECT_EQ(round_opt::round(0.25, 8, -1, RM::RTZ), 0.0);
 }
 
+TEST(RoundOpt, TestRoundFixedExamples) {
+    EXPECT_EQ(round_opt::round(0, 50, 1, std::nullopt, RM::RNE), 0.0);
+    EXPECT_EQ(round_opt::round(1, 0, 1, std::nullopt, RM::RNE), 1.0);
+    EXPECT_EQ(round_opt::round(3, 0, 1, std::nullopt, RM::RTZ), 2.0);
+    EXPECT_EQ(round_opt::round(-1, 0, 1, std::nullopt, RM::RNE), -1.0);
+    EXPECT_EQ(round_opt::round(-3, 0, 1, std::nullopt, RM::RTZ), -2.0);
+
+    EXPECT_EQ(round_opt::round(3, -2, 8, -1, RM::RNE), 1.0);
+    EXPECT_EQ(round_opt::round(3, -2, 8, -1, RM::RAZ), 1.0);
+    EXPECT_EQ(round_opt::round(3, -2, 8, -1, RM::RTZ), 0.0);
+
+    EXPECT_EQ(round_opt::round(2, -2, 8, -1, RM::RNE), 0.0);
+    EXPECT_EQ(round_opt::round(2, -2, 8, -1, RM::RAZ), 1.0);
+    EXPECT_EQ(round_opt::round(2, -2, 8, -1, RM::RTZ), 0.0);
+
+    EXPECT_EQ(round_opt::round(1, -2, 8, -1, RM::RNE), 0.0);
+    EXPECT_EQ(round_opt::round(1, -2, 8, -1, RM::RAZ), 1.0);
+    EXPECT_EQ(round_opt::round(1, -2, 8, -1, RM::RTZ), 0.0);
+}
+
+
 TEST(RoundOpt, TestRoundWithPrec) {
     std::vector<round_test_t> inputs = {
         // 8 * 2 ** -3 (representable)
@@ -74,6 +95,52 @@ TEST(RoundOpt, TestRoundWithPrec) {
     }
 }
 
+TEST(RoundOpt, TestRoundWithPrecFixed) {
+    std::vector<round_test_t> inputs = {
+        // 8 * 2 ** -3 (representable)
+        {-3, 8, -1, 2, RM::RNE}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RNA}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RTP}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RTN}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RTZ}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RAZ}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        // 9 * 2 ** -3 (below halfway)
+        {-3, 9, -1, 2, RM::RNE}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 2, RM::RNA}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 3, RM::RTP}, // 9 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 9, -1, 2, RM::RTN}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 2, RM::RTZ}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 3, RM::RAZ}, // 9 * 2 ** -3 => 1 * 3 ** -1 (up)
+        // 10 * 2 ** -3 (exactly halfway)
+        {-3, 10, -1, 2, RM::RNE}, // 10 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 10, -1, 3, RM::RNA}, // 10 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 10, -1, 3, RM::RTP}, // 10 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 10, -1, 2, RM::RTN}, // 10 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 10, -1, 2, RM::RTZ}, // 10 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 10, -1, 3, RM::RAZ}, // 10 * 2 ** -3 => 1 * 3 ** -1 (up)
+        // 11 * 2 ** -3 (above halfway)
+        {-3, 11, -1, 3, RM::RNE}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 11, -1, 3, RM::RNA}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 11, -1, 3, RM::RTP}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 11, -1, 2, RM::RTN}, // 11 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 11, -1, 2, RM::RTZ}, // 11 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 11, -1, 3, RM::RAZ}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        // 12 * 2 ** -3 (representable)
+        {-3, 12, -1, 3, RM::RNE}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RNA}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RTP}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RTN}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RTZ}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RAZ}, // 12 * 2 ** -3 => 1 * 3 ** -1
+    };
+
+    for (const auto& [exp_in, c_in, exp_out, c_out, rm] : inputs) {
+        const auto y_expect = static_cast<double>(fpy::RealFloat(false, exp_out, c_out));
+        const auto y = round_opt::round(c_in, exp_in, 2, std::nullopt, rm);
+        EXPECT_EQ(y, y_expect);
+    }
+}
+
 TEST(RoundOpt, TestRoundWithN) {
     std::vector<round_test_t> inputs = {
         // 8 * 2 ** -3 (representable)
@@ -117,6 +184,52 @@ TEST(RoundOpt, TestRoundWithN) {
         const auto x = static_cast<double>(fpy::RealFloat(false, exp_in, c_in));
         const auto y_expect = static_cast<double>(fpy::RealFloat(false, exp_out, c_out));
         const auto y = round_opt::round(x, 3, -2, rm);
+        EXPECT_EQ(y, y_expect);
+    }
+}
+
+TEST(RoundOpt, TestRoundWithNFixed) {
+    std::vector<round_test_t> inputs = {
+        // 8 * 2 ** -3 (representable)
+        {-3, 8, -1, 2, RM::RNE}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RNA}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RTP}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RTN}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RTZ}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        {-3, 8, -1, 2, RM::RAZ}, // 8 * 2 ** -3 => 1 * 2 ** -1
+        // 9 * 2 ** -3 (below halfway)
+        {-3, 9, -1, 2, RM::RNE}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 2, RM::RNA}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 3, RM::RTP}, // 9 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 9, -1, 2, RM::RTN}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 2, RM::RTZ}, // 9 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 9, -1, 3, RM::RAZ}, // 9 * 2 ** -3 => 1 * 3 ** -1 (up)
+        // 10 * 2 ** -3 (exactly halfway)
+        {-3, 10, -1, 2, RM::RNE}, // 10 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 10, -1, 3, RM::RNA}, // 10 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 10, -1, 3, RM::RTP}, // 10 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 10, -1, 2, RM::RTN}, // 10 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 10, -1, 2, RM::RTZ}, // 10 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 10, -1, 3, RM::RAZ}, // 10 * 2 ** -3 => 1 * 3 ** -1 (up)
+        // 11 * 2 ** -3 (above halfway)
+        {-3, 11, -1, 3, RM::RNE}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 11, -1, 3, RM::RNA}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 11, -1, 3, RM::RTP}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        {-3, 11, -1, 2, RM::RTN}, // 11 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 11, -1, 2, RM::RTZ}, // 11 * 2 ** -3 => 1 * 2 ** -1 (down)
+        {-3, 11, -1, 3, RM::RAZ}, // 11 * 2 ** -3 => 1 * 3 ** -1 (up)
+        // 12 * 2 ** -3 (representable)
+        {-3, 12, -1, 3, RM::RNE}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RNA}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RTP}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RTN}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RTZ}, // 12 * 2 ** -3 => 1 * 3 ** -1
+        {-3, 12, -1, 3, RM::RAZ}, // 12 * 2 ** -3 => 1 * 3 ** -1
+    };
+
+    for (const auto& [exp_in, c_in, exp_out, c_out, rm] : inputs) {
+        const auto y_expect = static_cast<double>(fpy::RealFloat(false, exp_out, c_out));
+        const auto y = round_opt::round(c_in, exp_in, 3, -2, rm);
         EXPECT_EQ(y, y_expect);
     }
 }
