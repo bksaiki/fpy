dcef1711e85c57aea8d3c5bab1323aa03b857b31
Brett Saiki <bksaiki@gmail.com>
2026-03-28 14:38:52 -0700

    remove old interpreter

--- fpy2/interpret/__init__.py
+++ fpy2/interpret/__init__.py
@@ -1,7 +1,6 @@
 """Interpreters for FPy."""
 
 from .byte import BytecodeInterpreter
-from .default import DefaultInterpreter
 from .interpreter import Interpreter, get_default_interpreter, set_default_interpreter
 
 set_default_interpreter(BytecodeInterpreter())
